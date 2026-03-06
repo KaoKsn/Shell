7b13ca85ce25b8e6cb7d014330e98de8c187e460
KaoKsn <mayurakrishna.25@proton.me>
2026-03-07 02:21:06 +0530

    Rename & Modularize main src

--- src/shell.c
+++ src/shell.c
@@ -1,22 +1,4 @@
-#include <signal.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "../include/command_list.h"
-#include "./built_ins.c"
-
-#define MAX_ARGLEN 128
-#define MAX_ARGS 16
-#define INPUT_BUFFER_SIZE MAX_ARGLEN * MAX_ARGS + MAX_ARGS - 1
-#define INVALID_INPUT 1
-
-int read_input(char *cmd);
-char **breakdown(char *cmd, int len, int *targs);
-void printargs(char **args, int targs);
-int execute(int cmd_id, char **cmdargs, int targs);
-void freecmdargs(char **args);
+#include "shell.h"
 
 static void sigint_handler(int s) 
 {
---
+++ src/shell.h
@@ -0,0 +1,25 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/command_list.h"
+#include "./built_ins.c"
+
+#define INVALID_INPUT 1
+#define MAX_ARGLEN 128
+#define MAX_ARGS 16
+#define INPUT_BUFFER_SIZE MAX_ARGLEN * MAX_ARGS + MAX_ARGS - 1
+
+int read_input(char *cmd);
+char **breakdown(char *cmd, int len, int *targs);
+void printargs(char **args, int targs);
+int execute(int cmd_id, char **cmdargs, int targs);
+void freecmdargs(char **args);
+
+#endif // !SHELL_H
+
